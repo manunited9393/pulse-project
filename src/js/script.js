5627e2c078b7398a11cde85dc2ea902f50f7ee97
Michail <man_united_9393@mail.ru>
2019-12-13 21:31:57 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -26,6 +26,13 @@ $(document).ready(function(){
       })
     })
 
-  
+      //modal
 
-});
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    })
+
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #order, #thanks').fadeOut();
+    })
+})
